b8e0891e4cc90dd91fb477c2916af2409c953832
INCRED_99 <devpathak9801@gmail.com>
2026-03-20 18:44:19 +0530

    postman responding.

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -1,7 +1,17 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser= asyncHandler( async(req , res)=>{
+    // get details from frontend
+    // check for validation (whether empty or no)
+    // check for user already existing (email/username)
+    // get image / avatar and upload on cloudinary
+    // create a user object on db
+    // remove password and refresh token from response 
+    // check from the response whether user created or not
+    // return res
 
+    const {fullname, email,password, username}=req.body
+    console.log("email is: ", email)
     res.status(200).json({
         message: "ok"
     })
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,17 @@
   import { Router } from "express"; 
   import { registerUser } from "../controllers/user.controllers.js";
-
+import {uploads} from "../middlewares/multer.middlewares.js"
   const router= Router() // mini express app that groups related routes in a separate file.
 
-  router.route("/register").post(registerUser)
+  router.route("/register").post(
+    uploads.fields([{
+      name: "avatar", 
+      maxCount: 1
+    },                              // file handling using multer middleware.
+  {
+    name: "coverImage", 
+    maxCount: 1
+  }]),registerUser)
 
   // router.route("/login").post(login)
 
